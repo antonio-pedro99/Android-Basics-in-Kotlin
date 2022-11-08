0c11e472a7fbaeb94937509ed9195bf54ba54f12
antonio-pedro99 <tonio.pedro99@gmail.com>
2022-11-08 12:44:44 +0530

    add Navigation Tests

---
+++ android-basics-kotlin-words-app-starter/app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -0,0 +1,21 @@
+package com.example.wordsapp
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Test
+
+class NavigationTests {
+
+    @Test
+    fun navigate_to_words_nav_component() {
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+        val letterListScenario = launchFragmentInContainer<LetterListFragment>(themeResId =
+        R.style.Theme_Words)
+
+      
+    }
+}
